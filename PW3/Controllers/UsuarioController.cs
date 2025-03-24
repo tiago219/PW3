d2c2132a920056d29185e73e62e367d1d28f23f2
20231350250 <20231350250@LTI-109-16>
2025-03-24 18:12:26 -0300

    controlers

---
+++ PW3/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PW3.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
