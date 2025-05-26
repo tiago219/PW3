07ed7f276d509f9c4aa084856adedbe951ecda5c
20231350250 <20231350250@LTI-109-16>
2025-05-26 18:12:25 -0300

    aaa

--- PW3/Controllers/DisciplinasController.cs
+++ PW3/Controllers/DisciplinasController.cs
@@ -1,12 +1,133 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+using PW3.Entidades;
 
 namespace PW3.Controllers
 {
     public class DisciplinasController : Controller
     {
-        public IActionResult Index()
+        private readonly string connectionString = "Server=localhost;Database=aulabd2;Uid=root;Pwd=;";
+        // GET: DisciplinasController
+        public ActionResult Index()
+        {
+            List<DisciplinaEntidade> model = new List<DisciplinaEntidade>();
+
+            DisciplinaEntidade item1 = new DisciplinaEntidade();
+            item1.Id = 1;
+            item1.Nome = "Programação Web";
+            item1.Ativo = true;
+            model.Add(item1);
+
+            DisciplinaEntidade item2 = new DisciplinaEntidade();
+            item1.Id = 2;
+            item1.Nome = "Educação Física";
+            item1.Ativo = true;
+            model.Add(item2);
+
+            return View(model);
+        }
+
+        // GET: DisciplinasController/Details/5
+        public ActionResult Details(int id)
         {
             return View();
+        }
+
+        // GET: DisciplinasController/Create
+        public ActionResult Create()
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    var comando = new MySqlCommand("Insert into Disciplinas (Nome) values (?)", connection);
+
+                    comando.Parameters.AddWithValue("?", dados.Nome);
+                    comando.ExecuteNonQuery();
+
+                    connection.Close();
+
+                }
+
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch (Exception ex) {
+            return View();
+        }
+
+        // POST: DisciplinasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DisciplinaEntidade dados)
+        {
+                try
+                {
+                    using (var connection = new MySqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        var comando = new MySqlCommand("Insert into Disciplinas (Nome) values (?)", connection);
+
+                        comando.Parameters.AddWithValue("?", dados.Nome);
+                        comando.ExecuteNonQuery();
+
+                        connection.Close();
+
+                    }
+
+                    return RedirectToAction(nameof(Index));
+
+                }
+                catch (Exception ex)
+                {
+                    {
+                return View();
+            }
+        }
+
+        // GET: DisciplinasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DisciplinasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DisciplinasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DisciplinasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ PW3/Entidades/DisciplinaEntidade.cs
@@ -0,0 +1,17 @@
+﻿namespace PW3.Entidades
+{
+    public class DisciplinaEntidade
+    {
+        /*
+         CREATE TABLE DISCIPLINAS(
+            Id INT PRIMARY KEY AUTO_INCREMENT,
+            Nome VARCHAR(150) NOT NULL,
+            Ativo BIT DEFAULT 1
+        )
+        */
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
